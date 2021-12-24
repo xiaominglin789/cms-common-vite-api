5e74bbd73d98cd4e27f719904334f37742f53750
xiaominglin789 <xiaominglin789@gmail.com>
2021-12-24 14:21:41 +0800

    fix: 修复密文解码封装错误

--- src/libs/crypto.js
+++ src/libs/crypto.js
@@ -11,27 +11,35 @@ class CryptoHelper {
   /**
    * aes加密
    * @param {*} key
+   * @param {*} type 加解码的类型,默认 aes-256-cbc
    */
-  encodeAES(msg) {
+   encodeAES(msg, type="aes-256-cbc") {
     if (!msg) return
-    
-    let crypted = ""
 
-    return  crypted
+    const cipherived = crypto.createCipheriv(
+      type,
+      String(process.env.AESKEY32),
+      String(process.env.AESIV16)
+    )
+
+    return  cipherived.update(msg, 'utf8', 'hex') + cipherived.final('hex')
   }
-  
+
   /**
    * aes解码
-   * @param {*} msg 
-   * @returns 
+   * @param {*} msg
+   * @param {*} type 加解码的类型,默认 aes-256-cbc
+   * @returns
    */
-  decodeAES(msg) {
+  decodeAES(msg, type="aes-256-cbc") {
     if(!msg) return
 
-    const cipher = crypto.createDecipheriv('aes-256-cbc',String(process.env.AESKEY32), String(process.env.AESIV16))
-    return  cipher.update(msg, 'base64', 'utf8') + cipher.final('utf8')
+    const cipher = crypto.createDecipheriv(
+      type,
+      String(process.env.AESKEY32),
+      String(process.env.AESIV16))
+    return  cipher.update(msg, 'hex', 'utf8') + cipher.final('utf8')
   }
-
 }
 
 module.exports = new CryptoHelper()
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,14 @@ const Router = require("koa-router")
 const Token = require("../libs/token")
 const router = new Router()
 const CryptoHelper = require("../libs/crypto")
+const Base64 = require("../libs/base64")
 
 // 接口
 router.get("/", async(ctx, next) => {
-  ctx.body = "hello"
+	const test = "123456"
+	console.log(test, " 加密后 ", CryptoHelper.encodeAES(test))
+
+	ctx.body = "hello"
 })
 
 router.post("/user/login", async(ctx, next) => {
@@ -13,13 +17,15 @@ router.post("/user/login", async(ctx, next) => {
   	username: "admin",
   	password: "123456"
   }
-  console.log(ctx.request.body.username)
-  const body = ctx.request.body
 
-	console.log("返回: ", ctx.request.body.username)
-	console.log(CryptoHelper.decodeAES(ctx.request.body.username))
+  const { username, password } = ctx.request.body
 
-  if (body.username === userInfo.username && body.password === userInfo.password) {
+	// console.log("username", username)
+  // console.log("password", password)
+
+  if (cryptUsernameHandle(username) === userInfo.username && 
+			cryptPasswordHandle(password) === userInfo.password) {
+    // 测试
 		const token = await Token.genralToken({ id: 1 })
 
   	ctx.status = 200
@@ -34,5 +40,38 @@ router.post("/user/login", async(ctx, next) => {
   	}
   }
 })
+
+
+/**
+ * 根据约定: 账号解密: aes-256-cbc
+ */
+function cryptUsernameHandle(msg) {
+  if (!msg) return
+
+  const res = CryptoHelper.decodeAES(msg)
+	console.log("username-code -> res： ", res);
+  return res
+}
+
+/**
+ * 根据约定: 密码解密: aes-256-cbc + base64
+ * 1.msg -> crypto decrypt
+ * 2.res -> base64 decode -> 明文
+ */
+function cryptPasswordHandle(msg) {
+  if (!msg) return
+
+	console.log("原始密文: ", msg);
+	// const msgLower = msg.toLowerCase(msg)
+	// console.log("msgLower: ", msgLower);
+
+	const res = CryptoHelper.decodeAES(msg)
+	console.log("aes 解码: ", res);
+
+	const msgBase64 = Base64.decode(res)
+	console.log("msgBase64 解码: ", msgBase64);
+	
+	return msgBase64
+}
 
 module.exports = router
